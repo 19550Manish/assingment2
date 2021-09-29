c683f9f44d3f188929ee76890cc43a74953980ba
19550Manish <91590633+19550Manish@users.noreply.github.com>
2021-09-29 16:15:30 +0530

    Update MainApp.java

--- Bank/src/MainApp.java
+++ Bank/src/MainApp.java
@@ -2,7 +2,12 @@
 public class MainApp {
 
 	public static void main(String[] args) {
-		Current current = new Current("ram") 
+		Current current = new Current("ram","delhi","new delhi",110086,50000.0f,"16-mar-19","active"); 
+       		current.ShowDetail();
+       		current.withdraw(600f);
+       
+       		current.deposite(5000f);
+       		current.ShowDetail();
 
 	}
 
